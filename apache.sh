12e338fe3c1cd04460c9e7b5a48de7b7be6caacf
stisdol612 <steven.tisdol@gmail.com>
2022-06-29 17:36:12 -0400

    Shell Changes

--- apache.sh
+++ apache.sh
@@ -3,5 +3,7 @@ sudo yum update -y
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
+sudo firewall-cmd — permanent — add-service=http
+sudo firewall-cmd — reload
 sudo amazon-linux-extras install epel -y
 sudo yum install stress -y
--- modules/compute/asg-scale.tf
+++ modules/compute/asg-scale.tf
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up_1" {
   namespace           = "AWS/EC2"
   period              = "120"
   statistic           = "Average"
-  threshold           = "90"
+  threshold           = "80"
 
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.auto_scaling_group_1.id
